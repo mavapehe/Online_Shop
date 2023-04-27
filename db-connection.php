8353d6833f3c055c737e01c65ea1237c042c8e2f
mavapehe <85805589+mavapehe@users.noreply.github.com>
2023-04-28 03:59:13 +1000

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -1,11 +1,10 @@
 <?php
 $host = 'containers-us-west-148.railway.app';
-$port = 6272;
 $user = 'root';
 $password = 'UOvUUOX46PGiKmDe43Y8';
 $database = 'products_db';
 
-$conn = new mysqli($host, $port, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
