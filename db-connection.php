800537a440797256ef65beda4c071242b39c9359
mavapehe <85805589+mavapehe@users.noreply.github.com>
2023-04-28 03:17:59 +1000

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -2,7 +2,7 @@
 $servername = "containers-us-west-148.railway.app";
 $username = "root";
 $password = "UOvUUOX46PGiKmDe43Y8";
-$dbname = "products_db";
+$dbname = "railway";
 $port = 6272;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
