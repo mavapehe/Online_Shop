413499fbc35abf312279bfc46fdf9e3ecba0f090
mavapehe <85805589+mavapehe@users.noreply.github.com>
2023-04-28 03:44:17 +1000

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -1,15 +1,13 @@
 <?php
-$host = 'containers-us-west-148.railway.app';
-$port = '6272';
-$db   = 'railway';
-$user = 'root';
-$pass = 'UOvUUOX46PGiKmDe43Y8';
-$charset = 'utf8mb4';
+$servername = "containers-us-west-148.railway.app";
+$username = "root";
+$password = "UOvUUOX46PGiKmDe43Y8";
+$dbname = "products_db";
+$port = 6272;
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
-try {
-    $pdo = new \PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
