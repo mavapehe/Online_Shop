9f1d39e1427481230b556b62e0dd340318e5820b
mavapehe <85805589+mavapehe@users.noreply.github.com>
2023-04-28 04:54:08 +1000

    Update meat-and-seafood.php

--- meat-and-seafood.php
+++ meat-and-seafood.php
@@ -36,16 +36,18 @@ require_once('back-button.php')
     <h2>Meat & Seafood</h2>
     <div class="products-grid">
 
-        <?php
-        require 'db-connection.php';
+<?php
+require 'db-connection.php';
 
-        $sql = "SELECT id, name, price, quantity, image FROM products WHERE id IN (2001, 2002, 2003, 2004)";
+$sql = "SELECT id, name, price, quantity, image FROM products_db WHERE id IN (8001, 8002, 8003, 8004)";
 
-        $result = $conn->query($sql);
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll();
 
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-        ?>
+if (!empty($result)) {
+    foreach($result as $row) {
+?>
         <a href="product.php?id=<?php echo $row['id']; ?>" class="product-link">
             <div class="product-item">
             <img src="/<?php echo $row["image"]; ?>" alt="<?php echo $row["name"]; ?>">
@@ -62,7 +64,6 @@ require_once('back-button.php')
         } else {
             echo "No products found.";
         }
-        $conn->close();
         ?>
 
     </div>
